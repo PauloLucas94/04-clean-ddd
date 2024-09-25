7a47ad123959c7a4c1905cf60175e2cf9b91cc45
Paulo Lucas <paulo.lucas@rocketmail.com>
2024-09-24 21:50:27 -0300

    ddd: create resources with eslintrc.json

--- src/domain/entities/value-objects/slug.ts
+++ src/domain/entities/value-objects/slug.ts
@@ -1,26 +1,26 @@
 export class Slug {
-    public value: string
-    constructor(value: string) {
-        this.value=value
-    }
-    /**
-     * Recieves a string and returns a Slug object
-     * 
-     * Example: "An example title" -> "an-example-title"
-     * @param text {string}
-     */
+  public value: string
+  constructor(value: string) {
+    this.value = value
+  }
+  /**
+   * Recieves a string and returns a Slug object
+   *
+   * Example: "An example title" -> "an-example-title"
+   * @param text {string}
+   */
 
-    static createFromText(text: string) {
-        const slugText = text
-            .normalize("NFKD")
-            .toLowerCase()
-            .trim()
-            .replace(/\s+/g, '-')
-            .replace(/[^\w-]+/g, '')
-            .replace(/_/g, '-')
-            .replace(/--+/g, '-')
-            .replace(/-$/g, '')
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
 
-        return new Slug(slugText)
-    }
-} 
+    return new Slug(slugText)
+  }
+}
